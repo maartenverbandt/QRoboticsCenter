d873ec52f7124040504bfca621a0f24343ccbcef
Maarten Verbandt,u0098208 <maarten.verbandt@kuleuven.be>
2016-08-26 16:53:36 +0200

    bug fix: kill non-existing timer

--- QRoboticsCenter/qrecorderwidget.cpp
+++ QRoboticsCenter/qrecorderwidget.cpp
@@ -7,7 +7,7 @@ QRecorderWidget::QRecorderWidget(QWidget *parent) : QLabel(parent)
     setFrameShape(StyledPanel);
     setMinimumSize(80,14);
     setAlignment(Qt::AlignCenter);
-    stop();
+    setStyleSheet("QLabel { background-color : Forestgreen; color : black;  border-radius : 4px; border-style: outset; border-width: 1px; border-color: gray;}");
 }
 
 void QRecorderWidget::timerEvent(QTimerEvent *)
