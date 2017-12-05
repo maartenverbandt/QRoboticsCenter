e16733ef18860958cd6eade6d4948ad0cc1e27ab
Maarten Verbandt <maarten.verbandt@hotmail.com>
2017-12-05 15:10:17 +0100

    remove all obsolete files

--- QRoboticsCenter/BalancingRobot/qbalancingmeasurementmanager.cpp
+++
@@ -1,8 +0,0 @@
-#include "qbalancingmeasurementmanager.h"
-
-QBalancingMeasurementManager::QBalancingMeasurementManager(QObject *parent) :
-    QRobotMeasurementManager(parent)
-{
-    _udp->addPort(new QPoseUdpWriter(_udp));
-    _udp->addPort(new QAttitudeUdpWriter(_udp));
-}
